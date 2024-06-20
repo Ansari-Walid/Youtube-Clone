dc1c171ee7af1fc11e3c2ffb68874069a7171850
Walid-Ansari <walidwasim26@gmail.com>
2024-06-20 21:22:06 +0530

    search results completed

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -11,8 +11,34 @@ const SearchResults = () => {
   const { searchQuery } = useParams();
   const { setLoading } = useContext(Context);
 
-  useEffect(() => {}, [searchQuery]);
-  return <div>SearchResults</div>;
+  useEffect(() => {
+    document.getElementById("root").classList.remove("custom-h");
+    fetchSearchResults();
+  }, [searchQuery]);
+
+  const fetchSearchResults = () => {
+    setLoading(true);
+    fetchDataFromApi(`search/?q=${searchQuery}`).then((res) => {
+      console.log(res);
+      setResults(res?.contents);
+      setLoading(false);
+    });
+  };
+  return (
+    <div className="flex flex-row h-[calc(100%-56px)]">
+      <LeftNav />
+      <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
+        <div className="grid grid-cols-1 gap-2 p-5">
+          {results?.map((item) => {
+            if (item.type !== "video") return false;
+            let video = item.video;
+
+            return <SearchResultVideoCard key={video?.videoId} video={video} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchResults;
