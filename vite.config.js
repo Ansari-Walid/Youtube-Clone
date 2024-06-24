5a8d07b184125ed520fa74f8d8953d4140a637f2
Walid-Ansari <walidwasim26@gmail.com>
2024-06-24 14:33:25 +0530

    Project Completed

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
-    build: {
-      outDir: "dist",
-    },
-    base: "/Youtube-Clone/",
+    base: "/Youtube-Clone/dist/",
     define: {
       "process.env.REACT_APP_YOUTUBE_API_KEY": JSON.stringify(
         env.REACT_APP_YOUTUBE_API_KEY
